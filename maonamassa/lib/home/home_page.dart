b78f9c44ed4f18f4bb2abc11de2b96b4b662b418
Alex Henrique <alex00henrique@gmail.com>
2024-12-27 06:23:43 -0800

    .

--- maonamassa/lib/home/home_page.dart
+++ maonamassa/lib/home/home_page.dart
@@ -5,6 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Nossa Primeira Home Page');
+    return Scaffold(body: Center(child: Text('Nossa Primeira Home Page')));
   }
 }
